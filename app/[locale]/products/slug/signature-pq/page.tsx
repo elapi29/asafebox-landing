8390e6c2c32a9a58a0511c7c85732627c86b3a8b
elapi29 <elapi.projects@gmail.com>
2025-10-11 00:36:51 +0200

    Fix build: use correct relative imports (../../../..) for Footer and SignaturePQDiagram

--- app/[locale]/products/slug/signature-pq/page.tsx
+++ app/[locale]/products/slug/signature-pq/page.tsx
@@ -1,7 +1,7 @@
 // app/[locale]/products/slug/signature-pq/page.tsx
 import Link from 'next/link'
-import Footer from '@/components/Footer'
-import SignaturePQDiagram from '@/components/illos/SignaturePQDiagram'
+import Footer from '../../../../../components/Footer'
+import SignaturePQDiagram from '../../../../../components/illos/SignaturePQDiagram'
 
 type Locale = 'es' | 'en' | 'de'
 
